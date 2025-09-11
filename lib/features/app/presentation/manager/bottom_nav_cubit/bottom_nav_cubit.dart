0ed2678ed1e0ee4d5dc4b00be547754763e68407
Odai Bishr <odaibishr@gmail.com>
2025-09-12 02:44:34 +0300

    create bottom nav cubit

---
+++ lib/features/app/presentation/manager/bottom_nav_cubit/bottom_nav_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'bottom_nav_state.dart';
+
+class BottomNavCubit extends Cubit<BottomNavState> {
+  BottomNavCubit() : super(BottomNavInitial());
+}
