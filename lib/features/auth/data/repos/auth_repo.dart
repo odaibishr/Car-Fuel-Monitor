9c390d4ec598e385803e582773415426f2de2f13
Odai Bishr <odaibishr@gmail.com>
2025-09-15 02:32:49 +0300

    create auth repository

---
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:car_monitor/core/errors/failure.dart';
+import 'package:car_monitor/features/auth/data/models/user_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserModel>> signUpWithEmailAndPassword(
+      String email, String password, String? name, String? phone);
+  Future<Either<Failure, UserModel>> signInWithEmailAndPassword(
+      String email, String password);
+  Future<void> signOut();
+}
