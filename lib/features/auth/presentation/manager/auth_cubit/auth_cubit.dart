56085e735f14f068a50ccd07a67060d2a8010d84
Odai Bishr <odaibishr@gmail.com>
2025-09-15 03:32:14 +0300

    implments auth methods in cubit

--- lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
@@ -1,9 +1,45 @@
 import 'package:bloc/bloc.dart';
+import 'package:car_monitor/core/errors/error_model.dart';
 import 'package:car_monitor/features/auth/data/models/user_model.dart';
-import 'package:meta/meta.dart';
+import 'package:car_monitor/features/auth/data/repos/auth_repo.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit(this.authRepo) : super(AuthInitial());
+  final AuthRepo authRepo;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    emit(AuthLoading());
+    try {
+      final user = await authRepo.signInWithEmailAndPassword(email, password);
+      user.fold((failure) => emit(AuthError(failure.errorMessage)),
+          (user) => emit(AuthAuthenticated(user: user)));
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
+
+  Future<void> signUpWithEmailAndPassword(
+      String email, String password, String? name, String? phone) async {
+    emit(AuthLoading());
+    try {
+      final user = await authRepo.signUpWithEmailAndPassword(
+          email, password, name, phone);
+      user.fold((failure) => emit(AuthError(failure.errorMessage)),
+          (user) => emit(AuthAuthenticated(user: user)));
+    } catch (e) {
+      return emit(AuthError(e.toString()));
+    }
+  }
+
+  Future<void> signOut() async {
+    emit(AuthLoading());
+    try {
+      await authRepo.signOut();
+      emit(AuthInitial());
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
 }
